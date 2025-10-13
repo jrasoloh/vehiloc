c253220c3f6654d2c21578857ad304a5a72aa478
jossy.rasoloharijaona <jossy.rasoloharijaona@openclassrooms.com>
2025-10-13 18:32:36 +0200

    feat(car): show car

---
+++ src/Controller/CarController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Car;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CarController extends AbstractController
+{
+    #[Route('/car/{id}', name: 'app_car_show', requirements: ['id' => '\d+'])]
+    public function show(Car $car): Response
+    {
+        return $this->render('car/show.html.twig', [
+            'car' => $car,
+        ]);
+    }
+}
