8fc00bf4eba3d962aee66fe70f23a111f4cc4703
jossy.rasoloharijaona <jossy.rasoloharijaona@openclassrooms.com>
2025-10-14 10:09:37 +0200

    feat(car): delete car

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Car;
+use App\Repository\CarRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,5 +17,20 @@ final class CarController extends AbstractController
         return $this->render('car/show.html.twig', [
             'car' => $car,
         ]);
+    }
+
+    #[Route('/car/{id}/delete', name: 'app_car_delete', requirements: ['id' => '\d+'])]
+    public function delete(int $id, CarRepository $carRepository, EntityManagerInterface $entityManager): Response
+    {
+        $car = $carRepository->find($id);
+
+        if (!$car) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $entityManager->remove($car);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_home');
     }
 }
